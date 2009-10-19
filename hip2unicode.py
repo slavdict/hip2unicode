118af4f2c968fa0f901aa10924c627a0ed5a7c72
while0pass <while0pass@yandex.ru>
2009-10-19 18:28:20 +0400

    Comments location in regexp dictionary has been changed

--- hip2unicode.py
+++ hip2unicode.py
@@ -8,16 +8,16 @@ cslav_regexps = {
     (script tag <::слав> or without any script tag
     if no script tag is in the document) """
 
+    # А -- \u0410 -- CYRILLIC CAPITAL LETTER A
     ur'[AА]'    # \u0041 LATIN CAPITAL LETTER A
     :           # \u0410 CYRILLIC CAPITAL LETTER A
-    ur'А',      # \u0410 CYRILLIC CAPITAL LETTER A
+    ur'А',
 
-
+    # а -- \u0430 -- CYRILLIC SMALL LETTER A
     ur'[aа]'    # \u0061 LATIN SMALL LETTER A
                 # \u0430 CYRILLIC SMALL LETTER A
     :
-    ur'а',      # \u0430 CYRILLIC SMALL LETTER A
-
+    ur'а',
  
 }
 
