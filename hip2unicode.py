c5feb18290c3ac0bb412a82d77ea435794a0df9f
while0pass <while0pass@yandex.ru>
2009-10-19 18:21:08 +0400

    added dictionary with regexps for character replacement in text excerpts typed in Church Slavonic script

--- hip2unicode.py
+++ hip2unicode.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-A = (
-    u'A', # \0041 LAT
-    u'А', # \0410 CYR
-)
-a = (
-    u'a', # \0061 LAT
-    u'а', # \0430 CYR
-)
+cslav_regexps = {
+    
+    """ Regular expressions for character replacement
+    in text marked as being typed in Church Slavonic script
+    (script tag <::слав> or without any script tag
+    if no script tag is in the document) """
+
+    ur'[AА]'    # \u0041 LATIN CAPITAL LETTER A
+    :           # \u0410 CYRILLIC CAPITAL LETTER A
+    ur'А',      # \u0410 CYRILLIC CAPITAL LETTER A
+
+
+    ur'[aа]'    # \u0061 LATIN SMALL LETTER A
+                # \u0430 CYRILLIC SMALL LETTER A
+    :
+    ur'а',      # \u0430 CYRILLIC SMALL LETTER A
+
+ 
+}
+
