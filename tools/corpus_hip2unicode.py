bcf04ecefdb723b5e9f40606fe8544fee2aa20ad
while0pass <while0pass@yandex.ru>
2009-11-10 04:32:34 +0300

    first working implementation

--- tools/corpus_hip2unicode.py
+++ tools/corpus_hip2unicode.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from hip2unicode.functions import hip2unicode as h2u
+from hip2unicode.tools.binary_converter import binary_converter
 import os
 import codecs
 
@@ -25,6 +26,12 @@ def all_files(path):
             result.extend([os.path.join(dirpath, filename) for filename in filenames])
     return result
 
+def make_all_folders(path):
+    p = os.path.dirname(path)
+    if not os.path.exists(p):
+        make_all_folders(p)
+        os.mkdir(p)
+
 file_list = all_files(corpus_path)
 if not file_list:
     print 'There is no file to convert in the corpus folder.'
@@ -34,28 +41,47 @@ if not file_list:
 # проверяем, существует ли папка corpus_unicode
 # если не существует, создаём её
 corpus_unicode_folder_exists = os.path.exists(corpus_unicode_path)
+
 if corpus_unicode_folder_exists:
     # проверяем, есть ли в ней файлы, и при наличии, удаляем их
-    file_list = all_files(corpus_unicode_path)
-    for f in file_list:
+    stuff_file_list = all_files(corpus_unicode_path)
+    for f in stuff_file_list:
         os.remove(f)
 else:
     os.mkdir(corpus_unicode_path)
 
-for old_path in file_list:
+enc_list = ['utf-8', 'cp1251', 'koi8-r']
+for file_path in file_list:
 
-    f = open(old_path) # codecs.open(old_path, 'rb', 'utf-8')
-    text = u''
-    for line in f:
-        text = u'%s%s' % (text, line)
+    # binary_converter(file_path)
+    
+    f = open(file_path)
+    e = False
+    for enc in enc_list:
+        global e
+        try:
+            text = f.read().decode(enc)
+        except UnicodeDecodeError:
+            continue
+            e = True
+        else:
+            break
+            e = False
+    if e:
+        print 'File "%s" is encoded with unknown encoding.' % file_path
+        print 'Known encodings are', enc_list 
+        raw_input('Press ENTER to exit program.')
+        quit()
+
     f.close()
 
-    converted_text = h2u(text)
+    converted_text = h2u(text).encode('utf-8')
 
-    new_path = old_path.replace(corpus_path, corpus_unicode_path)
+    new_path = file_path.replace(corpus_path, corpus_unicode_path)
 
+    make_all_folders(new_path)
     fu = open(new_path, 'w')
     fu.write(converted_text)
     fu.close()
 
-    print 'File "%s" is converted.' % old_path.replace(corpus_path, '')
+    print 'File "%s" is converted.' % file_path.replace(corpus_path, '')
