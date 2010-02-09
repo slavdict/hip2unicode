9141a4948587a058c96f6bbb82ca864092db361d
while0pass <while0pass@yandex.ru>
2010-02-09 08:06:49 +0300

    antconc representation added

---
+++ representations/antconc.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+""" Variable (attributes) names are all in Latin """
+
+Az = u'\u00cd'
+az = u'\u00ed'
+
+Buki = u'\u00ce'
+buki = u'\u00ee'
+
+Vedi = u'\u00cf'
+vedi = u'\u00ef'
+
+Vedi_Izhica = u'\u00d0'
+vedi_izhica = u'\u00f0'
+
+Glagolj = u'\u00d1'
+glagolj = u'\u00f1'
+
+Dobro = u'\u00d2'
+dobro = u'\u00f2'
+
+Estj = u'\u00d3' # == Estj_wide
+estj = u'\u00f3'
+Yatj = u'\u00d4'
+yatj = u'\u00f4'
+Estj_wide = Est
+estj_wide = u'\u00f5'
+
+Zhivete = u'\u00d6'
+zhivete = u'\u00f6'
+
+Zemlja = u'\u00d9'
+zemlja = u'\u00f9'
+Zelo = u'\u00da'
+zelo = u'\u00fa'
+
+Izhe = u'\u00db'
+izhe = u'\u00fb'
+I = u'\u00dc'
+i = u'\u00fc'
+I8 = Izhe
+i8 = izhe
+I10 = I
+i10 = i
+Izhica = u'\u00dd'
+izhica = u'\u00fd'
+
+
+
+paerok = ur'\\ъ'
+vedi_titlo = ur'\\в'
+glagol_titlo = ur'\\г'
+dobro_titlo = ur'\\д'
+on_titlo = ur'\\о'
+rcy_titlo = ur'\\р'
+slovo_titlo = ur'\\с'
+kher_titlo = ur'\\х'
+cherv_titlo = ur'\\ч'
+
+
