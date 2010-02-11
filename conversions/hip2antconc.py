6bc86ddd5466664e6a2987fc0d4fea2f053567c0
while0pass <while0pass@yandex.ru>
2010-02-11 20:45:04 +0300

    added antconc representation and hip2antconc conversion

---
+++ conversions/hip2antconc.py
@@ -0,0 +1,167 @@
+# -*- coding: UTF-8 -*-
+
+from hip2unicode.representations import hip
+from hip2unicode.representations import antconc, cslav
+
+hip2antconc = (
+# hip-нормализация
+# ...
+# удаление ненужной разметки
+{   # киноварь
+    ur'%<':   u'',
+    ur'%>':   u'',
+
+    ur'%\(':  u'',
+    ur'%\)':  u'',
+
+    ur'%\[':  u'',
+    ur'%\]':  u'',
+},
+{
+    ur'\([cсCС]\.\ *\d+\)':  u'', 
+},
+{   # сноски
+    # <сноска 1> ::= *{  <текст сноски> *  <текст сноски> }
+    ur' \*{ .*? \* .*? } ': u'', 
+    # <сноска 2> ::= **{ <текст сноски> ** <текст сноски> } 
+    ur''' \*\*{ .*?  \*\* .*?} ''' : u''
+},
+{   # звездочки для поющих на клиросе
+    ur'\*': u'',
+},
+{
+    
+    # Regular expressions for character replacement
+    # in text marked as being typed in Church Slavonic script
+    # (script tag <::слав> or without any script tag
+    # if no script tag is in the document)
+    hip.A : antconc.Az ,
+    hip.a : antconc.az ,
+
+    u'Б'  : antconc.Buki,
+    u'б'  : antconc.buki,
+ 
+    hip.B : antconc.Vedi ,
+    hip.b : antconc.vedi ,
+
+    u'Г'  : antconc.Glagolj,
+    u'г'  : antconc.glagolj,
+
+    u'Д'  : antconc.Dobro,
+    u'д'  : antconc.dobro,
+
+    u'Ж'  : antconc.Zhivete,
+    u'ж'  : antconc.zhivete,
+    
+    u'З'  : antconc.Zemlja,
+    u'з'  : antconc.zemlja,
+    u'S'  : antconc.Zelo,
+    u's'  : antconc.zelo,
+
+    u'И'  : antconc.Izhe,
+    u'и'  : antconc.izhe,
+    hip.i_without_dot : antconc.i,
+
+    u'Й'  : antconc.I_kratkoe,
+    u'й'  : antconc.i_kratkoe,
+
+    hip.K : antconc.Kako ,
+    hip.k : antconc.kako ,
+
+    u'Л'  : antconc.Ljudi,
+    u'л'  : antconc.ljudi,
+
+    hip.M : antconc.Myslete ,
+    hip.m : antconc.myslete ,
+
+    hip.H : antconc.Nash ,
+    hip.h : antconc.nash ,
+    
+    hip.O : antconc.On ,
+    hip.o : antconc.on ,
+
+    hip.P : antconc.Rcy ,
+    hip.p : antconc.rcy ,
+
+    hip.C : antconc.Slovo ,
+    hip.c : antconc.slovo ,
+
+    hip.T : antconc.Tverdo ,
+    hip.t : antconc.tverdo ,
+
+    hip.X : antconc.Kher ,
+    hip.x : antconc.kher ,
+
+    hip.V_double_gravis : antconc.Izhica ,
+    hip.v_double_gravis : antconc.izhica ,
+},
+{
+    hip.Wide_E : antconc.Estj_shirokoe ,
+    hip.wide_e : antconc.estj_shirokoe ,
+
+    u'I'  : antconc.I,
+    u'i'  : antconc.i,
+
+    hip.Yat : antconc.Jatj ,
+    hip.yat : antconc.jatj ,
+
+    hip.V : antconc.Izhica ,
+    hip.v : antconc.izhica ,
+
+    hip.Ksi : antconc.Ksi ,
+    hip.ksi : antconc.ksi ,
+
+    hip.Wide_O : antconc.On_shirokoe ,
+    hip.wide_o : antconc.on_shirokoe ,
+
+    hip.W : antconc.Omega ,
+    hip.w : antconc.omega ,
+
+    hip.Ole : antconc.Ole ,
+    hip.ole : antconc.ole ,
+
+    hip.Ot : antconc.Ot ,
+    hip.ot : antconc.ot ,
+
+    hip.Psi : antconc.Psi ,
+    hip.psi : antconc.psi ,
+
+    hip.F : antconc.Fita ,
+    hip.f : antconc.fita ,
+
+    hip.J_a : antconc.A_jotirovannoe ,
+    hip.j_a : antconc.a_jotirovannoe ,
+
+    hip.Ja : antconc.Jus_malyj ,
+    hip.ja : antconc.jus_malyj ,
+
+    hip.equal_sign : u'' ,
+    hip.single_quote : antconc.udarenie_ostroe ,
+    hip.back_single_quote : antconc.udarenie_tupoe ,
+    hip.caret : antconc.udarenie_oblechennoe ,
+    hip.tilde : antconc.titlo ,
+
+    hip.paerok : antconc.paerok ,
+    hip.vedi_titlo : antconc.vedi_titlo ,
+    hip.glagol_titlo : antconc.glagolj_titlo ,
+    hip.dobro_titlo : antconc.dobro_titlo ,
+    hip.on_titlo : antconc.on_titlo ,
+    hip.rcy_titlo : antconc.rcy_titlo ,
+    hip.slovo_titlo : antconc.slovo_titlo ,
+    hip.kher_titlo : antconc.kher_titlo ,
+    hip.cherv_titlo : antconc.chervj_titlo ,
+    
+},
+{
+    hip.Oy : antconc.Oy ,
+    hip.oy : antconc.oy ,
+
+    hip.E : antconc.Estj ,
+    hip.e : antconc.estj ,
+},
+{   
+    hip.Y : antconc.Uk ,
+    hip.y : antconc.uk ,
+
+},
+)
--- representations/antconc.py
+++ representations/antconc.py
@@ -3,17 +3,14 @@
 
 """ Variable (attributes) names are all in Latin """
 
-Az = u'\u00cd'
-az = u'\u00ed'
+Az = u'\u00ce'
+az = u'\u00ee'
 
-Buki = u'\u00ce'
-buki = u'\u00ee'
+Buki = u'\u00cf'
+buki = u'\u00ef'
 
-Vedi = u'\u00cf'
-vedi = u'\u00ef'
-
-# Vedi_Izhica = u'\u00d0'
-# vedi_izhica = u'\u00f0'
+Vedi = u'\u00d0'
+vedi = u'\u00f0'
 
 Glagolj = u'\u00d1'
 glagolj = u'\u00f1'
@@ -21,12 +18,12 @@ glagolj = u'\u00f1'
 Dobro = u'\u00d2'
 dobro = u'\u00f2'
 
-Estj = u'\u00d3' # == Estj_wide
+Estj = u'\u00d3' # == Estj_shirokoe
 estj = u'\u00f3'
-Yatj = u'\u00d4'
-yatj = u'\u00f4'
-Estj_wide = Est
-estj_wide = u'\u00f5'
+Jatj = u'\u00d4'
+jatj = u'\u00f4'
+Estj_shirokoe = Estj
+estj_shirokoe = u'\u00f5'
 
 Zhivete = u'\u00d6'
 zhivete = u'\u00f6'
@@ -44,20 +41,105 @@ I8 = Izhe
 i8 = izhe
 I10 = I
 i10 = i
-# Izhica = u'\u00dd'
-# izhica = u'\u00fd'
-Izhica = u'\u00d0'
-izhica = u'\u00f0'
+Izhica = u'\u00dd'
+izhica = u'\u00fd'
 
+I_kratkoe = u'\u0100'
+i_kratkoe = u'\u0101'
 
-paerok = ur'\\ъ'
-vedi_titlo = ur'\\в'
-glagol_titlo = ur'\\г'
-dobro_titlo = ur'\\д'
-on_titlo = ur'\\о'
-rcy_titlo = ur'\\р'
-slovo_titlo = ur'\\с'
-kher_titlo = ur'\\х'
-cherv_titlo = ur'\\ч'
+Kako = u'\u0102'
+kako = u'\u0103'
+Ksi = u'\u0104'
+ksi = u'\u0105'
 
+Ljudi = u'\u0106'
+ljudi = u'\u0107'
 
+Myslete = u'\u0108'
+myslete = u'\u0109'
+
+Nash = u'\u010a'
+nash = u'\u010b'
+
+On = u'\u010c'
+on = u'\u010d'
+Omega = u'\u0110'
+omega = u'\u0111'
+On_shirokoe = u'\u0112'
+on_shirokoe = u'\u0113'
+Ole = u'\u0114'
+ole = u'\u0115'
+Ot = u'\u0116'
+ot = u'\u0117'
+
+Pokoj = u'\u0118'
+pokoj = u'\u0119'
+Psi = u'\u011a'
+psi = u'\u011b'
+
+Rcy = u'\u011c'
+rcy = u'\u011d'
+
+Slovo = u'\u011e'
+slovo = u'\u011f'
+
+Tverdo = u'\u0120'
+tverdo = u'\u0121'
+
+Uk = u'\u0122'
+uk = u'\u0123'
+Uk_digraph = u'\u0124'
+uk_digraph = u'\u0125'
+
+Fert = u'\u0126'
+fert = u'\u0127'
+Fita = u'\u0128'
+fita = u'\u0129'
+
+Kher = u'\u014c'
+kher = u'\u014d'
+
+Cy = u'\u014e'
+cy = u'\u014f'
+
+Chervj = u'\u0150'
+chervj = u'\u0151'
+
+Sha = u'\u015a'
+sha = u'\u015b'
+
+Shcha = u'\u015c'
+shcha = u'\u015d'
+
+Er = u'\u0160'
+er = u'\u0161'
+
+Ery = u'\u0168'
+ery = u'\u0169'
+
+Erj = u'\u016a'
+erj = u'\u016b'
+
+Ju = u'\u016c'
+ju = u'\u016d'
+
+Jus_malyj = u'\u016e'
+jus_malyj = u'\u016f'
+
+A_jotirovannoe = u'\u0170'
+a_jotirovannoe = u'\u0171'
+
+udarenie_ostroe = u'\u00c0'
+udarenie_tupoe = u'\u00c1'
+udarenie_oblechennoe = u'\u00c2'
+# double_gravis = u'\u00c3'
+titlo = u'\u00c4'
+paerok = u'\u00c5'
+vedi_titlo = u'\u00c6'
+glagolj_titlo = u'\u00c7'
+dobro_titlo = u'\u00c8'
+on_titlo = u'\u00c9'
+rcy_titlo = u'\u00ca'
+slovo_titlo = u'\u00cb'
+kher_titlo = u'\u00cc'
+chervj_titlo = ur'\u00cd'
