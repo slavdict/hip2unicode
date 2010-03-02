76c4f0e0c05ab4cc1da51ed5c99c57eba3b08874
while0pass <while0pass@yandex.ru>
2010-03-02 15:38:01 +0300

    added tool for converting to UTF-8

---
+++ tools/to_utf8.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from hip2unicode.tools import corpus_converter
+
+# def corpus_converter(path=None, corpus_folder='corpus', converted_corpus_folder='converted_corpus', conversions=None):
+
+corpus_converter.corpus_converter(
+    converted_corpus_folder = 'corpus-utf-8',
+    )
