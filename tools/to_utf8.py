837afed1c58af60dd98c8d98284f93495df5d488
while0pass <while0pass@yandex.ru>
2010-03-02 17:03:42 +0300

    null conversion

--- tools/to_utf8.py
+++ tools/to_utf8.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 from hip2unicode.tools import corpus_converter
+from hip2unicode.functions import all_hip_conversions as ahc
+from hip2unicode.functions import make_conversion as mc
+#from hip2unicode.conversions import noconversion
 
 # def corpus_converter(path=None, corpus_folder='corpus', converted_corpus_folder='converted_corpus', conversions=None):
 
 corpus_converter.corpus_converter(
     converted_corpus_folder = 'corpus-utf-8',
+    conversions             =  ahc(
+                                    slav=mc(({},)),
+                                    rus= mc(({},)),
+                                    lat= mc(({},)),
+                                  ),
     )
