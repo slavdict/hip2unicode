76230b6588a1b54ae027caa5d04fe81a8573fe22
while0pass <while0pass@yandex.ru>
2010-06-03 00:31:49 +0400

    in RE module "initial()" function is added

--- representations/RE.py
+++ representations/RE.py
@@ -37,6 +37,10 @@ def right_context(*list_of_strings):
 def neg_right_context(*list_of_strings):
     return u'(?!%s)' % EMPTY_STRING.join(list_of_strings)
 
+def initial(*list_of_strings):
+    x = cat(*list_of_strings)
+    return u'((^%s)|(%s%s))' % (x, REPR_ENVIRON.NON_LETTERS, x)
+
 def initial_context(*list_of_strings):
     x = cat(*list_of_strings)
     return u'(?<=^%s)|(?<=%s%s)' % (x, REPR_ENVIRON.NON_LETTERS, x)
