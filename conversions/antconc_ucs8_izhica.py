1a3616c502040b2621836f3c0ea9f958c0d23786
while0pass <while0pass@yandex.ru>
2010-06-22 00:06:25 +0400

    antconc_ucs8: Добавлено правило для десятиричного И с двойным грависом

--- conversions/antconc_ucs8_izhica.py
+++ conversions/antconc_ucs8_izhica.py
@@ -3,15 +3,13 @@
 """
 Данное промежуточное преобразование ориентировано 
 на церковно-славянскую орфографию с чётким противопоставлением 
-"согласной" и "гласной" ижиц. 
+"согласной" и "гласной" ижиц, а также с последовательной
+простановкой двойного грависа над "i" без диакритики.
 
-Имеется в виду те случаи, когда над ижицей 
-в значении "в" не ставится ничего, 
-а над ижицей в значении "и" обязательно ставится 
-какой-нибудь диакритический знак 
-(в том числе придыхание в начале слова 
-или двойной гравис при безударной позиции 
-внутри слова).
+Касательно ижицы, имеются в виду те случаи, когда над ней в значении "в" не
+ставится ничего, а над ижицей в значении "и" обязательно ставится какой-нибудь
+диакритический знак (в том числе придыхание в начале слова или двойной гравис
+при безударной позиции внутри слова).
 """
 from hip2unicode.representations.RE import *
 from hip2unicode.representations.ucs8 import *
@@ -23,11 +21,16 @@ nlc_AE          = neg_left_context( token( AE ) )
 nlc_AE_DIA      = neg_left_context( token( AE ), token( VOWEL_DIACRITICS ) )
 
 conversion = (
-
+    
+    # Ижица с двойным грависом
     (nlc_AE + CAPITAL_IZHICA + nrc_DIACRITICS,      CAPITAL_IZHICA_DOUBLE_GRAVIS),
     (nlc_AE_DIA + CAPITAL_IZHICA + nrc_DIACRITICS,  CAPITAL_IZHICA_DOUBLE_GRAVIS),
 
     (nlc_AE + SMALL_IZHICA + nrc_DIACRITICS,        SMALL_IZHICA_DOUBLE_GRAVIS),
     (nlc_AE_DIA + SMALL_IZHICA + nrc_DIACRITICS,    SMALL_IZHICA_DOUBLE_GRAVIS),
+
+    # Десятиричное И с двойным грависом
+    (CAPITAL_I + nrc_DIACRITICS,                    u'\u0406'), 
+    (SMALL_I + nrc_DIACRITICS,                      u'\u0456'),
 
 )
