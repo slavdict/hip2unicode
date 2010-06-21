d0d5e3d453a44ded734ddf41a7049d1637ab51e8
while0pass <while0pass@yandex.ru>
2010-06-21 22:12:47 +0400

    Добавлен несколько правил конверсии antconc_ucs8_advanced

--- conversions/antconc_ucs8_advanced.py
+++ conversions/antconc_ucs8_advanced.py
@@ -24,6 +24,19 @@ ic_CAPITAL_VOWELS   = initial_context( token( CAPITAL_VOWELS ) )
 nrc_ACCENTS         = neg_right_context( token( ACCENTS ) )
 
 conversion = (
+    
+    (SMALL_JUS_MALYJ + SMALL_ASPIRATION_CIRKUMFLEX, u'\|'), # RegExp escaped pipe sign
+    (CAPITAL_AZ + CAPITAL_ASPIRATION_AKUT,          u'\u0403'),
+    (SMALL_AZ + SMALL_ASPIRATION_AKUT,              u'\u0453'),
+    (CAPITAL_DIGRAPH_UK + CAPITAL_ASPIRATION_AKUT,  u'\u040C'),
+    (CAPITAL_I_AZ + CAPITAL_ASPIRATION_AKUT,        u'\u040B'),
+    (CAPITAL_WIDE_ON + CAPITAL_ASPIRATION_AKUT,     u'\u040F'),
+    (SMALL_DIGRAPH_UK + SMALL_ASPIRATION_AKUT,      u'\u045C'),
+    (SMALL_I_AZ + SMALL_ASPIRATION_AKUT,            u'\u045B'),
+    (SMALL_WIDE_ON + SMALL_ASPIRATION_AKUT,         u'\u045F'),
+    (CAPITAL_I + CAPITAL_ASPIRATION_AKUT,           u'\u0408'),
+    (SMALL_I + SMALL_ASPIRATION_AKUT,               u'\u0458'),
+    (SMALL_I_AZ + SMALL_ASPIRATION_CIRKUMFLEX,      u'\u00B1'),
 
     (SMALL_ON + SMALL_AKUT,                 u'0'),
     (SMALL_ZHIVETE + SMALL_TITLO,           u'9'),
@@ -36,5 +49,26 @@ conversion = (
     (SMALL_I + SMALL_GRAVIS,                u'J'),
     (CAPITAL_I_AZ + SMALL_ASPIRATION,       u'K'),
     (SMALL_LJUDI + DOBRO_TITLO,             u'L'),
+    (CAPITAL_WIDE_ON + CAPITAL_ASPIRATION,  u'N'),
+    (SMALL_RCY + SMALL_TITLO,               u'R'),
+    (SMALL_JUS_MALYJ + SMALL_GRAVIS,        u'S'),
+    (SMALL_MONOGRAPH_UK + SMALL_GRAVIS,     u'Y'),
+    (SMALL_AZ + SMALL_AKUT,                 u'a'), # LATIN
+    (SMALL_ESTJ + SMALL_AKUT,               u'e'), # LATIN
+    (SMALL_ERY + SMALL_AKUT,                u'h'),
+    (SMALL_I + SMALL_AKUT,                  u'j'),
+    (SMALL_I_AZ + SMALL_ASPIRATION,         u'k'),
+    (SMALL_LJUDI + SMALL_TITLO,             u'l'),
+    (SMALL_WIDE_ON + SMALL_ASPIRATION,      u'n'),
+    (SMALL_RCY + SLOVO_TITLO,               u'r'),
+    (SMALL_JUS_MALYJ + SMALL_AKUT,          u's'),
+    (SMALL_MONOGRAPH_UK + SMALL_AKUT,       u'y'), # LATIN
+    (SMALL_MONOGRAPH_UK + SMALL_CIRKUMFLEKS,u'{'),
+    (SMALL_IZHE + SMALL_TITLO,              u'}'),
+    (SMALL_IZHICA + SMALL_AKUT,             u'\u0402'),
+    (SMALL_KSI + SMALL_TITLO,               u'\u2026'),
+    (SMALL_AZ + SMALL_CIRKUMFLEKS,          u'\u2020'),
+    (SMALL_I + SMALL_CIRKUMFLEKS,           u'\u2021'),
+    (SMALL_JUS_MALYJ + SMALL_CIRKUMFLEKS,   u'\u2030'),
 
 )
