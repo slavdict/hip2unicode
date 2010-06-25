5ae73be2f1c1cb5b9c634df29e7d744487a32150
while0pass <while0pass@yandex.ru>
2010-06-25 04:07:10 +0400

    Исправлена проблема ложного создания двойного грависа над ижицей.

--- conversions/antconc_ucs8_izhica.py
+++ conversions/antconc_ucs8_izhica.py
@@ -20,14 +20,21 @@ nrc_DIACRITICS  = neg_right_context( token( DIACRITICS ) )
 nlc_AE          = neg_left_context( token( AE ) )
 nlc_AE_DIA      = neg_left_context( token( AE ), token( VOWEL_DIACRITICS ) )
 
+temp_CAPITAL_IZHICA = u'\u7770'
+temp_SMALL_IZHICA   = u'\u7771'
+
 conversion = (
     
     # Ижица с двойным грависом
-    (nlc_AE + CAPITAL_IZHICA + nrc_DIACRITICS,      CAPITAL_IZHICA_DOUBLE_GRAVIS),
+    (nlc_AE + CAPITAL_IZHICA + nrc_DIACRITICS,      temp_CAPITAL_IZHICA),
     (nlc_AE_DIA + CAPITAL_IZHICA + nrc_DIACRITICS,  CAPITAL_IZHICA_DOUBLE_GRAVIS),
+    (nlc_AE_DIA + temp_CAPITAL_IZHICA,              CAPITAL_IZHICA_DOUBLE_GRAVIS),
+    (temp_CAPITAL_IZHICA,                           CAPITAL_IZHICA),
 
-    (nlc_AE + SMALL_IZHICA + nrc_DIACRITICS,        SMALL_IZHICA_DOUBLE_GRAVIS),
+    (nlc_AE + SMALL_IZHICA + nrc_DIACRITICS,        temp_SMALL_IZHICA),
     (nlc_AE_DIA + SMALL_IZHICA + nrc_DIACRITICS,    SMALL_IZHICA_DOUBLE_GRAVIS),
+    (nlc_AE_DIA + temp_SMALL_IZHICA,                SMALL_IZHICA_DOUBLE_GRAVIS),
+    (temp_SMALL_IZHICA,                             SMALL_IZHICA),
 
     # Десятиричное И с двойным грависом
     (CAPITAL_I + nrc_DIACRITICS,                    u'\u0406'), 
