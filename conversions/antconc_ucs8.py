74eb7a58ff779d2c54174fa7b6c345359b0805e7
while0pass <while0pass@yandex.ru>
2010-06-28 02:49:54 +0400

    Добавлена конвертация сносок и коментариев.

--- conversions/antconc_ucs8.py
+++ conversions/antconc_ucs8.py
@@ -11,8 +11,12 @@ texts corpus.
 
 """
 
+from hip2unicode.conversions.antconc_ucs8_pre import conversion as pre_conversion
 from hip2unicode.conversions.antconc_ucs8_basic import conversion as basic_conversion
 from hip2unicode.conversions.antconc_ucs8_izhica import conversion as izhica_conversion
 from hip2unicode.conversions.antconc_ucs8_advanced import conversion as advanced_conversion
 
-conversion = basic_conversion + izhica_conversion + advanced_conversion
+conversion =    pre_conversion +    \
+                basic_conversion +  \
+                izhica_conversion + \
+                advanced_conversion
