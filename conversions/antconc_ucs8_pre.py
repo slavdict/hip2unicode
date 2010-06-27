fed7a2817290ee76092a0792cf1ac0beff830d04
while0pass <while0pass@yandex.ru>
2010-06-28 02:49:54 +0400

    Добавлена конвертация сносок и коментариев.

---
+++ conversions/antconc_ucs8_pre.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+from hip2unicode.representations.RE import *
+from hip2unicode.representations.antconc import *
+
+conversion = (
+
+    # Сноски и комментарии у нас в текстах заключаются
+    # в фигурные скобки, поэтому заменяем их на 
+    # двойные круглые скобки при этом перед открывающей
+    # круглой скобкой добавляем пробел.
+    (u'\{',  u'\u0020(('),
+    (u'\}',  u'))'),
+
+)
