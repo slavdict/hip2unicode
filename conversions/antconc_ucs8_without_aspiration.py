f516b0393ed29106b6a1fcdf152946755aa8be8a
while0pass <while0pass@yandex.ru>
2010-09-21 14:12:29 +0400

    В конвертации без придыханий импорт производился из неправильных модулей.

--- conversions/antconc_ucs8_without_aspiration.py
+++ conversions/antconc_ucs8_without_aspiration.py
@@ -9,9 +9,9 @@ Intentended use is converting affixes.
 
 """
 
-from hip2unicode.conversions.antconc_ucs8_basic import conversion as basic_conversion_without_aspiration
+from hip2unicode.conversions.antconc_ucs8_basic_without_aspiration import conversion as basic_conversion_without_aspiration
 from hip2unicode.conversions.antconc_ucs8_izhica import conversion as izhica_conversion
-from hip2unicode.conversions.antconc_ucs8_advanced import conversion as advanced_conversion_without_aspiration
+from hip2unicode.conversions.antconc_ucs8_advanced_without_aspiration import conversion as advanced_conversion_without_aspiration
 
 conversion =    basic_conversion_without_aspiration +  \
                 izhica_conversion + \
