84407e61b6af4f045ca575f837145536503723fe
while0pass <while0pass@yandex.ru>
2012-12-20 19:07:03 +0400

    pep8, чистка и рефакторинг кода, улучшения:
    - возможность указать папку, в которой находятся файлы для конвертации,
      в качестве консольного аргумента
    - правильное создание пути отконвертированного файла.

--- tools/corpus_converter.py
+++ tools/corpus_converter.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
-from hip2unicode.functions import hip2unicode as h2u
-#from hip2unicode.tools.binary_converter import binary_converter
 import os
-import codecs
+import sys
+
+from hip2unicode.functions import hip2unicode
 
 
-def corpus_converter(path=None, corpus_folder='corpus', converted_corpus_folder='converted_corpus', conversions=None):
+def corpus_converter(path=None, corpus_folder='corpus',
+        converted_corpus_folder='converted_corpus', conversions=None):
 
     if not path:
         path = os.path.abspath(os.getcwd())
-    
-    print '\nCurrent folder is "%s".' % path
+
+    print '\nCurrent folder is {0}'.format(path)
 
     corpus_path = os.path.join(path, corpus_folder)
     converted_corpus_path = os.path.join(path, converted_corpus_folder)
 
     # проверяем наличие папки corpus в текущем каталоге
-    
     if not os.path.exists(corpus_path):
-        print 'Corpus folder with the name "%s" does not exist.' % corpus_folder
+        print ('Corpus folder with the name "{0}" '
+               'does not exist.').format(corpus_folder)
         raw_input('Press ENTER to exit program.')
-        quit()
+        sys.exit(-1)
 
     def all_files(path):
         result = []
         for dirpath, subdirs, filenames in os.walk(path):
             if filenames:
-                result.extend([os.path.join(dirpath, filename) for filename in filenames])
+                result.extend(
+                          os.path.join(dirpath, filename)
+                          for filename in filenames)
         return result
 
     def make_all_folders(path):
@@ -39,57 +42,41 @@ def corpus_converter(path=None, corpus_folder='corpus', converted_corpus_folder=
     if not file_list:
         print 'There is no file to convert in the corpus folder.'
         raw_input('Press ENTER to exit program.')
-        quit()
+        sys.exit(-1)
 
-    # проверяем, существует ли папка converted_corpus
-    # если не существует, создаём её
-    converted_corpus_folder_exists = os.path.exists(converted_corpus_path)
-
-    if converted_corpus_folder_exists:
-        # проверяем, есть ли в ней файлы, и при наличии, удаляем их
-        stuff_file_list = all_files(converted_corpus_path)
-        for f in stuff_file_list:
+    # Проверяем, существует ли папка converted_corpus
+    # если не существует, создаём её.
+    if os.path.exists(converted_corpus_path):
+        # Проверяем, есть ли в ней файлы. Если да, удаляем их.
+        for f in all_files(converted_corpus_path):
             os.remove(f)
     else:
-        os.mkdir(converted_corpus_path)
+        make_all_folders(converted_corpus_path)
 
-    enc_list = ['utf-8', 'cp1251', 'koi8-r', ]
-    print 'Converting files ',
+    enc_list = ('utf-8', 'cp1251', 'koi8-r')
+    print '\nConverting files '
     for file_path in file_list:
-
-        # binary_converter(file_path)
-        
-        for enc in enc_list:
-            with open(file_path) as f:
+        with open(file_path) as f:
+            contents = f.read()
+            for enc in enc_list:
                 try:
-                    text = f.read().decode(enc)
+                    text = contents.decode(enc)
                 except UnicodeDecodeError:
-                    e = True
                     continue
                 else:
-                    print '%s\t\t%s' % (file_path.replace(corpus_path, '.'), enc)
-                    e = False
+                    print '{0}\t{1}'.format(enc,
+                                  os.path.relpath(file_path, corpus_path))
                     break
-        if e:
-            print 'File "%s" is encoded with unknown encoding.' % file_path
-            print 'Known encodings are', enc_list 
-            raw_input('Press ENTER to exit program.')
-            quit()
+            else:
+                print 'File "%s" is encoded with unknown encoding.' % file_path
+                print 'Known encodings are', enc_list
+                raw_input('Press ENTER to exit program.')
+                sys.exit(-1)
 
-        f.close()
-
-        converted_text = h2u(text, conversions).encode('utf-8')
-
-        new_path = file_path.replace(corpus_path, converted_corpus_path)
-        
-        # меняем все расширения на TXT
-        new_path = '%(file_path)s%(ext_sep)s%(ext)s' % {
-            'file_path': os.path.splitext(new_path)[0],
-            'ext_sep': os.extsep,
-            'ext': 'txt'
-            }
-
+        converted_text = hip2unicode(text, conversions).encode('utf-8')
+        new_path = os.path.relpath(file_path, corpus_path)
+        new_path = os.path.splitext(new_path)[0] + '.txt'
+        new_path = os.path.join(converted_corpus_path, new_path)
         make_all_folders(new_path)
-        fu = open(new_path, 'w')
-        fu.write(converted_text)
-        fu.close()
+        with open(new_path, 'w') as fu:
+            fu.write(converted_text)
--- tools/to_civilrus.py
+++ tools/to_civilrus.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
-from hip2unicode.tools import corpus_converter
-from hip2unicode.functions import all_hip_conversions as ahc
-from hip2unicode.functions import compile_conversion as cc
+import sys
+
 from hip2unicode.conversions import hip_civilrus
+from hip2unicode.functions import all_hip_conversions
+from hip2unicode.functions import compile_conversion
+from hip2unicode.tools import corpus_converter
 
-# def corpus_converter(path=None, corpus_folder='corpus', converted_corpus_folder='converted_corpus', conversions=None):
+conversions = {
+    'slav': compile_conversion(hip_civilrus.conversion),
+    'rus': 'delete',
+    'lat': 'delete',
+    'grec': 'delete',
+}
+args = {
+    'converted_corpus_folder': 'corpus-civilrus',
+    'conversions': all_hip_conversions(**conversions),
+}
+corpus_folder = None
 
-corpus_converter.corpus_converter(
-    converted_corpus_folder = 'corpus-civilrus',
-    conversions             =  ahc(
-                                    slav=cc(hip_civilrus.conversion),
-                                    rus='delete',
-                                    lat='delete',
-                                    grec='delete',
-                                  ),
-    )
+if len(sys.argv) == 2:
+    corpus_folder = sys.argv[1]
+
+if corpus_folder:
+    args['corpus_folder'] = corpus_folder
+
+corpus_converter.corpus_converter(**args)
