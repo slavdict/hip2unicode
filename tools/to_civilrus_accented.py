0975ce314eb93ee7d2b264dd3e870030a7bbc2f3
while0pass <while0pass@yandex.ru>
2013-04-30 01:19:04 +0400

    Добавлена конвертация из hip в гражданскую орфография с сохранением и унификацией ударений

---
+++ tools/to_civilrus_accented.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from hip2unicode.conversions import hip_civilrus_accented
+from hip2unicode.functions import all_hip_conversions
+from hip2unicode.functions import compile_conversion
+from hip2unicode.tools import corpus_converter
+
+conversions = {
+    'slav': compile_conversion(hip_civilrus_accented.conversion),
+    'rus': 'delete',
+    'lat': 'delete',
+    'grec': 'delete',
+}
+args = {
+    'converted_corpus_folder': 'corpus-civilrus',
+    'conversions': all_hip_conversions(**conversions),
+}
+corpus_folder = None
+converted_corpus_folder = None
+
+if len(sys.argv) > 1:
+    corpus_folder = sys.argv[1]
+if corpus_folder:
+    args['corpus_folder'] = corpus_folder
+
+if len(sys.argv) == 3:
+    converted_corpus_folder = sys.argv[2]
+if converted_corpus_folder:
+    args['converted_corpus_folder'] = converted_corpus_folder
+
+corpus_converter.corpus_converter(**args)
