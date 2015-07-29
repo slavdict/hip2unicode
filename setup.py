8ef470781091114dd2d9261219ab13ce1cdf42b6
Malo Skrylevo <majioa@yandex.ru>
2015-07-29 19:35:37 +0300

    Added setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='HIP2Unicode',
+    version='1.0',
+    description='Converter from HIP to Unicode, written in Python',
+    author='Andrej Hitrov',
+    author_email='andrej@hitrov.ru',
+    url='https://github.com/while0pass/hip2unicode',
+    license='Unlicensed',
+    package_dir = {'': 'hip2unicode'},
+    packages = [ 'hip2unicode',
+       'hip2unicode.conversions',
+       'hip2unicode.representations',
+       'hip2unicode.tools' ],
+)
