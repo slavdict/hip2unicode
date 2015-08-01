26532db415d1054c4edebab406bfd4f831662e2d
Nurono <while0pass@yandex.ru>
2015-08-02 00:29:47 +0300

    Попытка наведения минипорядка в этой старой файлокодопомойке.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
+# coding: utf-8
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='HIP2Unicode',
-    version='1.0',
-    description='Converter from HIP to Unicode, written in Python',
-    author='Andrej Hitrov',
-    author_email='andrej@hitrov.ru',
+    version='1.0.0',
+    description='Converter from HIP to Unicode',
+    author='Nurono',
+    author_email='while0pass@yandex.ru',
     url='https://github.com/while0pass/hip2unicode',
-    packages=[
-        'hip2unicode', 'hip2unicode.conversions', 'hip2unicode.representations',
-        'hip2unicode.tools'
-    ],
+    packages=['hip2unicode', 'hip2unicode.conversions',
+              'hip2unicode.representations'],
     zip_safe=False,
 )
