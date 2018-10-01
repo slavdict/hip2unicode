d01d7016b3b3b5b03f8034aa2ece4baad008def5
Nurono <while0pass@yandex.ru>
2018-10-01 13:20:35 +0300

    fix repo url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Converter from HIP to Unicode',
     author='Nurono',
     author_email='while0pass@yandex.ru',
-    url='https://github.com/while0pass/hip2unicode',
+    url='https://github.com/slavdict/hip2unicode',
     packages=['hip2unicode', 'hip2unicode.conversions',
               'hip2unicode.representations'],
     zip_safe=False,
